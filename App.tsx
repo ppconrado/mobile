afaea54c65f94c4b9f31e52483a1b33691379d18
ppconrado <ppconrado@yahoo.com.br>
2022-05-08 07:53:03 -0300

    widget, fonts, styles

--- App.tsx
+++ App.tsx
@@ -1,25 +1,31 @@
+import React from "react";
+import "react-native-gesture-handler";
+
 import { StatusBar } from "expo-status-bar";
 import { View } from "react-native";
 
-import AppLoading from "expo-app-loading";
 import {
   useFonts,
   Inter_400Regular,
   Inter_500Medium,
 } from "@expo-google-fonts/inter";
+import * as SplashScreen from "expo-splash-screen";
 
 import { theme } from "./src/theme";
-import { Widget } from "./src/components/Widget";
+import Widget from "./src/components/Widget";
 
 export default function App() {
+  SplashScreen.preventAutoHideAsync();
   const [fontsLoaded] = useFonts({
     Inter_400Regular,
     Inter_500Medium,
   });
 
   if (!fontsLoaded) {
-    return <AppLoading />;
+    return null;
   }
+
+  SplashScreen.hideAsync();
   return (
     <View
       style={{
@@ -27,8 +33,8 @@ export default function App() {
         backgroundColor: theme.colors.background,
       }}
     >
-      <Widget />
       <StatusBar style="light" backgroundColor="transparent" translucent />
+      <Widget />
     </View>
   );
 }
